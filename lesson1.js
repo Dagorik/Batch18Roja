98d92e9eeb155d1af432ea3b59fc9e6d7c3d1f38
Dagorik <dagorik@gmail.com>
2018-01-31 14:12:20 -0600

    first commit

---
+++ lesson1.js
@@ -0,0 +1,19 @@
+var x = 2
+var y = "hola"
+var z = 3.4
+var w = false
+
+var suma = 3 + 2
+var div = 100/2
+var res = 100%6
+var exp = 5**2
+console.log(typeof(exp))
+console.log(exp)
+
+
+> es mayor
+< es menor
+>= es mayor e igual
+<= es menor e igual
+== es igual
+!= es diferente
